69466467bee31c1b52dd2e4eeffff1513160c4ab
Juan Martinez <martinezalonsojn@gmail.com>
2023-11-01 19:53:50 +0100

    Add template and utilities

---
+++ template.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+#define TESTCASES true
+void solve(int tt) {
+    // <-- Solution goes here -->//
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    if (!TESTCASES) {
+        solve(0);
+        return 0;
+    }
+
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++) {
+        solve(i + 1);
+    }
+    return 0;
+}
---
+++ utils.sh
@@ -0,0 +1,50 @@
+#! /bin/bash
+
+new() {
+    if [ "$#" -ne 1 ]; then
+        echo "Please provide problem id"
+        return 1
+    fi
+
+    problem=$1
+    letter=${problem:(-1)}
+    number=${problem:0:(-1)}
+
+    mkdir -p "$number/$letter"
+    cp template.cpp "$number/$letter/$letter.cpp"
+
+    touch "$number/$letter/in"
+    touch "$number/$letter/out"
+
+    echo "Created folder for exercise $problem!"
+    echo "Make sure to copy the example input and output"
+}
+
+test() {
+    if [ "$#" -ne 1]; then
+        echo "Please provide problem id"
+        return 1
+    fi
+
+    problem=$1
+    letter=${problem:(-1)}
+    number=${problem:0:(-1)}
+
+    set -e
+
+    cd "$number/$letter"
+
+    g++ -std=c++17 "$letter.cpp" -o "$letter.out"
+    ./"$letter.out" <"in" >"tmp"
+
+    echo "Diff: (got | expected)"
+
+    diff "tmp" "out"
+
+    rm "tmp"
+
+    echo "Done!"
+}
+
+export new
+export test
